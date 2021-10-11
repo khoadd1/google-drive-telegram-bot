af45ff90fcdaf3e9f73c616e52274af39c7a3ac1
khoadd1 <49783890+khoadd1@users.noreply.github.com>
2021-10-11 22:05:39 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "2097193405:AAFDbIs9Rb7exceLVc7rq0oluOuOBp0QPVY"
     APP_ID = "8061384"
     API_HASH = "427cba9d9c75859e01180435b4913a68"
-    DATABASE_URL = "postgres://mnyixmrjgtcbme:c0a176fa281e9accd89dbef964fe277b64eafbf686ec619ee7a5c8e3944ffc3c@ec2-18-214-214-252.compute-1.amazonaws.com:5432/dfbjo6dahr6q0u"
+    DATABASE_URL =  "postgres://mnyixmrjgtcbme:c0a176fa281e9accd89dbef964fe277b64eafbf686ec619ee7a5c8e3944ffc3c@ec2-18-214-214-252.compute-1.amazonaws.com:5432/dfbjo6dahr6q0u"
     SUDO_USERS = "2042466088" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/joinchat/pdNhMocp98wyMDE1"
     DOWNLOAD_DIRECTORY = "./downloads/"
