bbb61f7449d35d5c77f7b39d7d956a80cbea4c97
khoadd1 <49783890+khoadd1@users.noreply.github.com>
2021-10-11 20:58:49 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,15 +1,15 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
+    BOT_TOKEN = "2097193405:AAHNOAs5o7QJJejB6uGj5IbZNG-dvMmwlOc"
+    APP_ID = "8061384"
+    API_HASH = "427cba9d9c75859e01180435b4913a68"
+    DATABASE_URL = "postgres://mnyixmrjgtcbme:c0a176fa281e9accd89dbef964fe277b64eafbf686ec619ee7a5c8e3944ffc3c@ec2-18-214-214-252.compute-1.amazonaws.com:5432/dfbjo6dahr6q0u"
+    SUDO_USERS = "2042466088 1233176715" # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/joinchat/pdNhMocp98wyMDE1"
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "388093006051-otsmlpedmt4617fcg9oqg3ighue4r32l.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "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"
 
-
+    
 class BotCommands:
   Download = ['download', 'dl']
   Authorize = ['auth', 'authorize']
